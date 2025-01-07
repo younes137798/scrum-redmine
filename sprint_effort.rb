a658e08959d33b290648e75d729e0f8d5b64a9f0
younes137798 <younesmirzajany101@gmail.com>
2025-01-07 10:06:39 +0330

    Add files via upload

---
+++ sprint_effort.rb
@@ -0,0 +1,13 @@
+# Copyright © Emilio González Montaña
+# Licence: Attribution & no derivatives
+#   * Attribution to the plugin web page URL should be done if you want to use it.
+#     https://redmine.ociotec.com/projects/redmine-plugin-scrum
+#   * No derivatives of this plugin (or partial) are allowed.
+# Take a look to licence.txt file at plugin root folder for further details.
+
+class SprintEffort < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :sprint
+
+end
